c5b826d6ef50ab575f7b7bdb4ba0128dfbe4bce5
key <zookyan@163.com>
2018-09-25 21:30:10 +0800

    Fizz&Buzz

--- src/main/java/test/FizzAndBuzz.java
+++ src/main/java/test/FizzAndBuzz.java
@@ -8,8 +8,9 @@ public class FizzAndBuzz {
 
     /**
      * fizz & buzz
+     * stage 1
      */
-    public static List<String> fizzBuzz(int n) {
+    public static List<String> fizzBuzzStage1(int n) {
         List<String> result = new ArrayList<String>(n);
         for (int i = 1; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
@@ -17,6 +18,24 @@ public class FizzAndBuzz {
             } else {
                 if (i % 3 == 0) result.add("Fizz");
                 else if (i % 5 == 0) result.add("Buzz");
+                else result.add(i + "");
+            }
+        }
+        return result;
+    }
+
+    /**
+     * fizz & buzz
+     * stage 2
+     */
+    public static List<String> fizzBuzzStage2(int n) {
+        List<String> result = new ArrayList<String>(n);
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0 || String.valueOf(i).contains("3") || String.valueOf(i).contains("5")) {
+                result.add("FizzBuzz");
+            } else {
+                if (i % 3 == 0 || String.valueOf(i).contains("3")) result.add("Fizz");
+                else if (i % 5 == 0 || String.valueOf(i).contains("5")) result.add("Buzz");
                 else result.add(i + "");
             }
         }
--- src/test/java/test/FizzAndBuzzTest.java
+++ src/test/java/test/FizzAndBuzzTest.java
@@ -4,12 +4,21 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static test.FizzAndBuzz.fizzBuzz;
+import static test.FizzAndBuzz.fizzBuzzStage1;
+import static test.FizzAndBuzz.fizzBuzzStage2;
 
 public class FizzAndBuzzTest {
     @Test
-    public void test(){
-        List<String> list = fizzBuzz(100);
+    public void test1(){
+        List<String> list = fizzBuzzStage1(100);
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
+    @Test
+    public void test2(){
+        List<String> list = fizzBuzzStage2(100);
         for (String s : list) {
             System.out.println(s);
         }
